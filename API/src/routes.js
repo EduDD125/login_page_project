28bb7ebb5921dda47a8f8dc6f2e5e0f618ff1ab6
Eduardo Derisso <dudu.derisso@gmail.com>
2024-08-14 19:48:36 -0300

    feat[routes]: gravando informações em arquivo externos, simulando banco de dados

--- API/src/routes.js
+++ API/src/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const fs = require('fs')
 
 const routes = express.Router(); 
 
 const users = [{
-    id:1,
     nome:'User',
     email:'user@gmail.com',
     password:'123456'
@@ -12,11 +12,39 @@ const users = [{
 routes.post('/login', (req, res) => {
     const {email, password} = req.body
 
-    user = users.find(user => user.email == email && user.password == password)
+    const user = users.find(user => user.email == email && user.password == password)
     
     if(user) return res.status(200).json(user);
     else return res.status(401).json({message: 'Credencial Inválida'});
     
+})
+
+
+routes.post("/create", (req, res) => {
+    const {name, email, password} = req.body;
+    const body = {
+        name: name,
+        email: email,
+        password: password
+    };
+
+    if (true) { // fazer lógica de criação de usuários únicos
+
+        fs.writeFile('users.json', JSON.stringify(body), (err) => {
+            if(err) {
+                res.status(500).json({
+                    status: 500,
+                    error: err.message || 'Internal server error'
+                });
+            } else {
+                res.status(201).json({
+                    status: 201,
+                    message: `User ${name} created`
+                });
+                console.log('The file has been saved!');
+            }
+        })
+    }
 })
 
 module.exports = routes;
