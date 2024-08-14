355d488e30578f6c772fac16a5fe8770cdda02b0
Eduardo Derisso <dudu.derisso@gmail.com>
2024-08-14 14:46:59 -0300

    feat[App, index, App style ]: update in the front layout to send the login data

--- frontend/login_page_project/src/loginPage/login.js
+++ frontend/login_page_project/src/loginPage/login.js
@@ -1,7 +1,16 @@
 export default function Login() {
     return(
-        <>
-        Login
-        </>
+        <div className="App">
+            <header className="App-header">
+                <div className="login-form-wrap">
+                    <h2>Login</h2>
+                    <form className="login-form">
+                        <input type="email" name="email" placeholder="Digite seu email" required></input>
+                        <input type="password" name="password" placeholder="Digite sua senha" required></input>
+                        <button type="submit" className="btn-login" >Enviar</button>
+                    </form>
+                </div>
+            </header>
+        </div>
     );
 }
