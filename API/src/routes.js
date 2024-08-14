1d789d3bdce8e8a0fd35da2f2e4a95338ae5a99d
Eduardo Derisso <dudu.derisso@gmail.com>
2024-08-13 22:12:54 -0300

    refactore[routes]: corrected console log to better explain routes modules functions

--- API/src/routes.js
+++ API/src/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = express.Router(); 
 
 routes.post('/login', (req, res) => {
-    console.log("Post endpoint");
+    console.log("Post endpoint at /login page");
 })
 
 module.exports = routes;
