4cd7b4cab356651a09d3bce0c0850210385664e7
Eduardo Derisso <dudu.derisso@gmail.com>
2024-08-16 16:27:54 -0300

    refactore[server routes]: removing some console logs

--- API/src/routes.js
+++ API/src/routes.js
@@ -10,12 +10,10 @@ routes.post('/login', (req, res) => {
 
     fs.readFile(path.join(__dirname,'../users.json'), 'utf8', (err, data) => {
         if(err){
-            console.log(err)
             return res.status(500).json({message: 'Erro ao ler arquivo'});
         }
 
         const savedUser = JSON.parse(data);
-        console.log(data)
 
         if(savedUser.email == email && savedUser.password == password) {
             return res.status(200).json(savedUser);
@@ -32,21 +30,18 @@ routes.post("/create", (req, res) => {
         email: email,
         password: password
     };
-    console.log("########################")
     if (!name || !email || !password ) return res.status(400).json({message:`Parametros de criação estão alsentes`})  
     fs.writeFile('users.json', JSON.stringify(newUser), (err) => {
         if(err) {
-            res.status(500).json({
+            return res.status(500).json({
                 status: 500,
                 error: err.message || 'Internal server error'
             });
         } 
         else {
-            res.status(201).json({
+            return res.status(201).json({
                 message: `User ${name} created`
             });
-            console.log('The file has been saved!');
-            console.log(newUser);
         }
     })
 })
